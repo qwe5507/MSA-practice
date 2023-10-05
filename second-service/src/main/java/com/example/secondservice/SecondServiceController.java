fc80cb9ab0c63213af44bb90bf406a24e6b5e551
qwe5507 <apvmffkdls@gmail.com>
2023-10-06 00:31:02 +0900

    요청 매핑 변경

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/second-service")
 @Slf4j
 public class SecondServiceController {
     @GetMapping("/welcome")
