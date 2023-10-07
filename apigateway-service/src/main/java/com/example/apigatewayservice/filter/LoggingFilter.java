08c4a762cb5a38a99fddbf882954eb2dc2eb28b1
qwe5507 <apvmffkdls@gmail.com>
2023-10-07 22:03:17 +0900

    Spring Cloud Gateway Logging Filter 적용
    OrderedGatewayFilter 사용하여 필터 우선순위 지정

--- apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/LoggingFilter.java
@@ -3,7 +3,9 @@ package com.example.apigatewayservice.filter;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -11,31 +13,32 @@ import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
-public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
-    public GlobalFilter() {
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+    public LoggingFilter() {
         super(Config.class);
     }
 
     @Override
     public GatewayFilter apply(final Config config) {
-        // Custom Pre Filter
 
-        return ((exchange, chain) -> {
+        GatewayFilter filter = new OrderedGatewayFilter((exchange, chain) -> {
             final ServerHttpRequest request = exchange.getRequest();
             final ServerHttpResponse response = exchange.getResponse();
 
-            log.info("Global Filter baseMessage: {}", config.getBaseMessage());
+            log.info("Logging Filter baseMessage: {}", config.getBaseMessage());
 
             if (config.isPreLogger()) {
-                log.info("Global Filter Start: request id => {}", request.getId());
+                log.info("Logging Filter Start: request uri => {}", request.getURI());
             }
-            //Custom Post Filter
+
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 if (config.isPostLogger()) {
-                    log.info("Global Filter End: response code => {}", response.getStatusCode());
+                    log.info("Logging Filter End: response code => {}", response.getStatusCode());
                 }
             }));
-        });
+        }, Ordered.LOWEST_PRECEDENCE); //  가장 낮은 우선순위
+
+        return filter;
     }
 
     @Data
