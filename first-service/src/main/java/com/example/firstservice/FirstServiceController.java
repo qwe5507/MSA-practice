f2d8fa78856dfdcd26b43bc86f6595fc19d35e45
qwe5507 <apvmffkdls@gmail.com>
2023-10-08 12:21:15 +0900

    Spring Cloud Gateway Load Balancer 등록

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,15 +1,21 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+
+    @Value("local.server.port")
+    private String serverPort;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +29,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server Port={}", request.getServerPort());
+        return String.format("Hi, there. This is a message from First Service on PORT %s.", serverPort);
     }
 }
