0e83091d0d06e1cb83b11021eb5ed3d867d710ce
qwe5507 <apvmffkdls@gmail.com>
2023-12-24 12:31:55 +0900

    fix: 컨슈머 직렬화 클래스 변경

--- order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducerConfig.java
+++ order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package com.example.orderservice.messagequeue;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -20,8 +21,8 @@ public class KafkaProducerConfig {
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> properties = new HashMap<>();
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(properties);
     }
